06f1affee89022f2e2fbfe24be92ca6da7303139
Therapho <51427300+Therapho@users.noreply.github.com>
2021-09-27 15:08:49 -0400

    Cleaned up extra imports, and additional props.

--- src/home/components/childDashboard/childDashboard.tsx
+++ src/home/components/childDashboard/childDashboard.tsx
@@ -1,4 +1,5 @@
 import { Stack } from "@fluentui/react";
+import { useAccount } from "../../../common/stores/account/queries/useAccount";
 import AccountCard from "../AccountCard/accountCard";
 import BalanceCard from "../BalanceCard/balanceCard";
 import TaskCard from "../TaskCard/taskCard";
@@ -6,20 +7,23 @@ import TransactionCard from "../TransactionCard/transactionCard";
 import { childDashBoardStyles } from "./childDashboard.styles";
 
 export const ChildDashboard = () => {
+  const { data: account } = useAccount();
   return (
     <section>
-      <Stack
-        horizontalAlign="center"
-        wrap
-        horizontal
-        tokens={childDashBoardStyles.stackTokens}
-        styles={childDashBoardStyles.stackStyles}
-      >
-        <AccountCard />
-        {/* <BalanceCard /> */}
-        {/* <TaskCard />
-        <TransactionCard /> */}
-      </Stack>
+      {account && (
+        <Stack
+          horizontalAlign="center"
+          wrap
+          horizontal
+          tokens={childDashBoardStyles.stackTokens}
+          styles={childDashBoardStyles.stackStyles}
+        >
+          <AccountCard />
+          <BalanceCard account={account} />
+          <TaskCard account={account} />
+          <TransactionCard account={account} />
+        </Stack>
+      )}
     </section>
   );
 };
