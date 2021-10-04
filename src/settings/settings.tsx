1f5cd271b302c8b2e5362c65ed46cb1ac4692f5c
Therapho <51427300+Therapho@users.noreply.github.com>
2021-10-04 16:32:27 -0400

    Added settings details

--- src/settings/settings.tsx
+++ src/settings/settings.tsx
@@ -1,10 +1,16 @@
-import { Text } from "@fluentui/react";
+import { Label, Text } from "@fluentui/react";
+import Card from "../common/components/card/card";
+import { useProfile } from "../common/stores/profile/queries/useProfile";
 export const Settings = () => {
  
-
+  const {data:profile} = useProfile();
   return(
   <main>
-    <Text> Settings</Text>
+    <h1> Settings</h1>
+    <Card>
+      <Label>User ID:</Label>
+      {profile?.userId} 
+    </Card>
     </main>
   )
   };
